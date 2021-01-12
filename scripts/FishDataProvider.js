b6aa522a18c9ac05a779e41cda1f7164ccf4d7ec
kjk1325e <kaitlinjkelley@gmail.comm>
2021-01-12 14:36:43 -0600

    Added JS requirements from ch.8 and ch.9

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,29 @@
+const fishCollection = [
+    {
+        name: "Bart",
+        food: "crustaceans",
+        species: "Amphiprioninae",
+        location: "Great Barrier Reef, Australia",
+        length: 2
+    },
+
+    {
+        name: "Marlin",
+        food: "crustaceans",
+        species: "Angel Fish",
+        location: "Atlantic Ocean",
+        length: 5
+    },
+
+    {
+        name: "Marlina",
+        food: "crustaceans",
+        species: "Lion Fish",
+        location: "Pacifice Ocean",
+        length: 3
+    }
+]
+
+export const useFish = () => {
+    return fishCollection.slice()
+}
