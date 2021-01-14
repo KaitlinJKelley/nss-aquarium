5084e88135af76ce71fbffd126484acf876903f2
kjk1325e <kaitlinjkelley@gmail.comm>
2021-01-14 11:08:14 -0600

    Completed ch13 functions to filter fish

--- scripts/fish/FishDataProvider.js
+++ scripts/fish/FishDataProvider.js
@@ -5,7 +5,7 @@ const fishCollection = [
         food: "Favorite Food: shrimp",
         species: "Species: Mickey Mouse Platy",
         location: "Home: Guatemala",
-        length: "Length: 2 inches"
+        length: 2
     },
 
     {
@@ -14,7 +14,7 @@ const fishCollection = [
         food: "Favorite Food: algae",
         species: "Species: Mekong Giant Catfish (Endangered)",
         location: "Home: China",
-        length: "Length: 9 feet"
+        length: 108
     },
 
     {
@@ -23,7 +23,7 @@ const fishCollection = [
         food: "Favorite Food: Smaller Fish",
         species: "Species: Lion Fish (Invasive)",
         location: "Home: Indo-Pacific Region",
-        length: "Length: 18 inches"
+        length: 18
     },
 
     {
@@ -32,7 +32,7 @@ const fishCollection = [
         food: "Favorite Food: Smaller Fish",
         species: "Species: Goliath Tiger Fish",
         location: "Home: Congo River Basin",
-        length: "Length: 5 feet"
+        length: 60
     },
 
     {
@@ -41,10 +41,46 @@ const fishCollection = [
         food: "Favorite Food: Smaller Fish and Crustaceans",
         species: "Species: Donkey Fish",
         location: "Home: All Over",
-        length: "Length: 30 inches"
+        length: 30
     }
 ]
 
 export const useFish = () => {
     return fishCollection.slice()
+}
+
+export const mostHolyFish = () => {
+    // 3, 6, 9, 12, etc... fish
+    const holyFish = []
+
+    for (const holy of fishCollection) {
+        if (holy.length % 3 === 0) {
+            holyFish.push(holy)
+        }
+    }
+    return holyFish
+}
+
+export const soldierFish = () => {
+    // 5, 10, 15, 20, 25, etc... fish
+    const soldiers = []
+
+    for (const soldier of fishCollection) {
+        if (fish.length % 5 === 0) {
+            soldiers.push(soldier)
+        }
+    }
+    return soldiers
+}
+
+export const nonHolyFish = () => {
+    // Any fish not a multiple of 3 or 5
+    const regularFish = []
+
+    for (const reg of fishCollection) {
+        if (reg.length % 3 !== 0 && reg.length % 5 !== 0) {
+            regularFish.push(reg)
+        }
+    }
+    return regularFish
 }
